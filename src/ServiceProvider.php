e91cce9e13c454f2690db09d3c032b3c20fd6f0e
David Négrier <d.negrier@thecodingmachine.com>
2016-04-14 22:02:16 +0200

    Switching to non-static getServices method returning callables (see #5 and #16)

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,10 +2,26 @@
 
 namespace Interop\Container;
 
+/**
+ * Classes implementing this interface are service providers. Then can provide one or many entries to a container.
+ */
 interface ServiceProvider
 {
     /**
-     * @return array
+     * Returns a list of all container entries registered by this service provider.
+     *
+     * - the key is the entry name
+     * - the value is a callable that will return the entry, aka the **factory**
+     *
+     * Factories have the following signature:
+     *        function(ContainerInterface $container, callable $getPrevious = null)
+     *
+     * About factories parameters:
+     *
+     * - the container (instance of `Interop\Container\ContainerInterface`)
+     * - a callable that returns the previous entry if overriding a previous entry, or `null` if not
+     *
+     * @return callable[]
      */
-    public static function getServices();
+    public function getServices();
 }
